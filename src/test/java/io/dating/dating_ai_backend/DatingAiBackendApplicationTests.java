cef646d780c81b9178913b20652d2cc1673f1100
prasanthravulaml@gmail.com <prasanthravulaml@gmail.com>
2024-11-12 10:23:05 -0600

    first commit

---
+++ src/test/java/io/dating/dating_ai_backend/DatingAiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.dating.dating_ai_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatingAiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
